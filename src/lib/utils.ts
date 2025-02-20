41568d70563aec84d2c23ab478887e1e8af0b1d1
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-02-20 22:57:57 +0530

    chore: latex add (#13)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
 import { ResumeData } from '@/types/resume';
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
