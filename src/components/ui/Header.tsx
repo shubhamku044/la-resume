4c92ea6afb310ceab055e4c2f74deac4cb376e23
shubhamku044 <shubhamku044@gmail.com>
2025-02-23 11:14:21 +0530

    chore: loader fix

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,24 +1,30 @@
 import Link from 'next/link';
-import { FaGithub } from 'react-icons/fa';
+import { LuGithub } from 'react-icons/lu';
+
 const Header = () => {
   return (
-    <header className="flex items-center justify-between border-b bg-white px-6 py-4">
-      <Link href="/" className="text-lg font-semibold transition hover:opacity-80">
-        La Resume
-      </Link>
+    <header className="bg-white">
+      <div className="mx-auto flex max-w-5xl items-center justify-between border-b px-6 py-4">
+        <Link href="/" className="text-lg font-semibold transition hover:opacity-80">
+          La Resume
+        </Link>
 
-      {/* Navigation Links */}
-      <nav className="flex items-center gap-6">
-        <Link href="/templates" className="text-gray-700 hover:text-black">
-          Templates
-        </Link>
-        <Link href="/user-details" className="text-gray-700 hover:text-black">
-          User Details
-        </Link>
-        <Link href="https://github.com/shubhamku044/la-resume" target="_blank">
-          <FaGithub className="size-8" />
-        </Link>
-      </nav>
+        <nav className="flex items-center gap-6 text-sm">
+          <Link href="/templates" className="text-gray-700 hover:text-black">
+            Templates
+          </Link>
+          <Link href="/user-details" className="text-gray-700 hover:text-black">
+            User Details
+          </Link>
+          <Link
+            className="rounded-full border-1 border-gray-300 p-2"
+            href="https://github.com/shubhamku044/la-resume"
+            target="_blank"
+          >
+            <LuGithub className="size-4" />
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 };
