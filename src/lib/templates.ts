6e6cbb0ebaf1e1c24ff0c95f46f5430f8913bfc3
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-02-25 17:52:22 +0530

    refactor: update sb2nov template image format and clean up sample data (#55)

--- src/lib/templates.ts
+++ src/lib/templates.ts
@@ -3,6 +3,6 @@ export const templates = [
     id: 'sb2nov',
     name: 'RenderCV Resume',
     description: 'A CV/resume theme of RenderCV.',
-    image: '/templates/sb2nov.png',
+    image: '/templates/sb2nov.webp',
   },
 ];
