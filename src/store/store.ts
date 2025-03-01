88875136cbd1936b2d915a74cd1b315b68a7a2e2
shubhamku044 <shubhamku044@gmail.com>
2025-03-02 01:54:55 +0530

    fix: new user with no data

--- src/store/store.ts
+++ src/store/store.ts
@@ -48,11 +48,13 @@ export const store = configureStore({
       },
     })
       .concat(userDetailsApi.middleware)
-      .concat(personalInfoApi.middleware)
-      .concat(() => (next) => (action) => {
-        console.log('RTK Query action: ', action);
-        return next(action);
-      });
+      .concat(personalInfoApi.middleware);
+    /*
+    .concat(() => (next) => (action) => {
+      console.log('RTK Query action: ', action);
+      return next(action);
+    });
+  */
   },
 });
 
